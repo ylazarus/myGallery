96a42dc7f0860fd71a59c350743013dac355a779
Yoni Lazarus <ylazarus@gmail.com>
2022-02-07 16:17:02 +0200

    Until Email Section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,90 @@
 'use strict'
 
-function onInit(){
+function onInit() {
     renderProjects()
+    renderModals()
 }
 
 
-function renderProjects(){
-    getProjects()
-
+function renderProjects() {
+    var projects = getProjects()
+    var strHtmls = []
+    for (let i = 0; i < projects.length; i++) {
+        var strHtml = `
+    <div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${i + 1}">
+      <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+      </div>
+      <img class="img-fluid" src="img/portfolio/0${i + 1}-thumbnail.jpg" alt="">
+    </a>
+    <div class="portfolio-caption">
+      <h4>${projects[i].name}</h4>
+      <p class="text-muted">${projects[i].title}</p>
+    </div>
+  </div>
+    `
+        strHtmls.push(strHtml)
+    }
+    $('.projects').html(strHtmls)
 }
 
 
+function renderModals() {
+    var projects = getProjects()
+    var strHtmls = []
+    for (let i = 0; i < projects.length; i++) {
+        var strHtml = `
+    <div class="portfolio-modal modal fade" id="portfolioModal${i + 1}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <!-- Project Details Go Here -->
+                <h2>${projects[i].name}</h2>
+                <p class="item-intro text-muted">${projects[i].title}</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/0${i + 1}-full.jpg" alt="">
+                <p>${projects[i].desc}</p>
+                <ul class="list-inline">
+                  <li>Date: ${projects[i].publishedAt}</li>
+                  <li>url: ${projects[i].url}</li>
+                  <a href="${projects[i].url}" target="_blank" class="btn btn-primary "
+                         tabindex="-1" role="button" aria-disabled="true">Check it out!</a>
+
+                  <li>Labels: ${projects[i].labels.join(' ')}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+        strHtmls.push(strHtml)
+    }
+    $('.modal-container').html(strHtmls)
+}
+
+function onSendEmail(){
+    var eladdress = $('input[name=email-address]')
+    var address = eladdress.value
+    var subject = $('input[name=subject]')
+    var message = $('input[name=email-body]')
+    console.log(address, subject, message);
+}
 
 
 
--- js/service.js
+++ js/service.js
@@ -1,31 +1,27 @@
 'use strict'
 
-gProjs = [
+var gProjs = [
     {
         id: 'minesweeper',
         name: 'Minesweeper',
         title: 'Project sprint 1 CA',
         desc: 'Self written in Vanilla JS with css styling',
         url: 'https://ylazarus.github.io/minesweeper/',
-        publishedAt: Date.now(),
-        labels: ['Matrixes', 'games', '']
-    }
+        publishedAt: 'January 2022',
+        labels: ['#matrixes', '#games']
+    },
     {
         id: 'pac-man',
         name: 'PacMan',
-        title: 'My own PacMan game',
+        title: 'My own PacMan',
         desc: 'Self written in Vanilla JS with css styling',
         url: 'https://ylazarus.github.io/pacManGame/',
-        publishedAt: Date.now(),
-        labels: ['Matrixes', 'keyboard events']
+        publishedAt: 'January 2022',
+        labels: ['#matrixes', '#keyboard events']
     }
 ]
 
-// createProjs()
 
-// function createProjs(){
-
-// }
 
 function getProjects() {
     return gProjs
